21032edea8f62647cae45a6b0ff7236e8162fcd3
Lisa Boulard <lisa.boulard@edu.esiee.fr>
2025-11-17 16:55:27 +0000

    Une première version

--- room.py
+++ room.py
@@ -1,7 +1,22 @@
 # Define the Room class.
 
 class Room:
+    """
+    La classe représente toutes les salles disponibles dans le jeu.
 
+    Attributes:
+        name (str): The command word.
+        description (str): The help string.
+        exits (function): The action to execute when the command is called.
+        number_of_parameters (int): The number of parameters expected by the command.
+
+    Methods:
+        __init__(self, name, descfription) : The constructor.
+        get_exit(self, direction) : Define the get_exit method.
+        get_exit_string(self) : Return a string describing the room's exits.
+        get_long_description(self): Return a long description of this room including exits.
+
+    """
     # Define the constructor. 
     def __init__(self, name, description):
         self.name = name
@@ -10,7 +25,19 @@ class Room:
     
     # Define the get_exit method.
     def get_exit(self, direction):
+        """
+        Move the player in the direction specified by the parameter.
+        The parameter must be a cardinal direction (N, E, S, O).
 
+        Args:
+            game (Game): The game object.
+            list_of_words (list): The list of words in the command.
+            number_of_parameters (int): The number of parameters expected by the command.
+
+        Returns:
+            None si la direction n'existe pas, sinon retourne 
+
+        """
         # Return the room in the given direction if it exists.
         if direction in self.exits.keys():
             return self.exits[direction]
